7b36e7f7fd03314308813a11b9e3824017c48cff
legion <kuhazubat33azest@outlook.com>
2024-01-07 20:24:52 -0500

    Breaking out functions

--- main.go
+++ main.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "strconv"
+	"strconv"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.LoadHTMLGlob("templates/*")
-  r.GET("/", func(c *gin.Context){
-    c.HTML(200, "index.tmpl", gin.H{
-        "title":"Main Website",
-    })
-  })
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.tmpl", gin.H{
+			"title": "Main Website",
+		})
+	})
 
-  r.GET("/newPlayer", func (c *gin.Context){
-    // TODO: Create sqlite connection to create players
-    c.HTML(200, "newPlayer.tmpl", gin.H{})
-  })
+	r.GET("/newPlayer", func(c *gin.Context) {
+		// TODO: Create sqlite connection to create players
+		c.HTML(200, "newPlayer.tmpl", gin.H{})
+	})
 
-  r.GET("/cancel", func (c *gin.Context){
-    c.HTML(200, "", gin.H{})
-  })
+	r.GET("/cancel", func(c *gin.Context) {
+		c.HTML(200, "", gin.H{})
+	})
 
-  r.GET("/round/:round", func (c *gin.Context){
-    // TODO: Extract this to db
-    var rounds = []struct {
-      description string
-      contract string
-    }{
-      {"Deal 10 cards", "2 Sets"},
-      {"Deal 10 cards", "1 Set and 1 Sequence"},
-      {"Deal 10 cards", "2 Sequence's"},
-      {"Deal 10 cards", "3 Sets"},
-      {"Deal 12 cards", "2 Sets and 1 Sequence"},
-      {"Deal 12 cards", "1 Set and 2 Sequence's"},
-      {"Deal 12 cards", "Three Sequence's"},
-    }
-    round := c.Param("round")
+	r.GET("/round/:round", func(c *gin.Context) {
+		// TODO: Extract this to db
 
-    nextRound := 0
-    i, err := strconv.Atoi(round)
-    if err != nil {
-      nextRound = 0
-    }
-    nextRound = i + 1
+		var rounds = []struct {
+			description string
+			contract    string
+		}{
+			{"Deal 10 cards", "2 Sets"},
+			{"Deal 10 cards", "1 Set and 1 Sequence"},
+			{"Deal 10 cards", "2 Sequence's"},
+			{"Deal 10 cards", "3 Sets"},
+			{"Deal 12 cards", "2 Sets and 1 Sequence"},
+			{"Deal 12 cards", "1 Set and 2 Sequence's"},
+			{"Deal 12 cards", "Three Sequence's"},
+		}
+		round := c.Param("round")
 
-    if nextRound > 7 {
-      nextRound = 1
-    }
+		nextRound := 0
+		i, err := strconv.Atoi(round)
+		if err != nil {
+			nextRound = 0
+		}
+		nextRound = i + 1
 
-    c.HTML(200, "roundPlay.tmpl", gin.H{
-      "round": i,
-      "description":rounds[i-1].description,
-      "contract": rounds[i-1].contract,
-      "nextRound": nextRound,
-    })
+		if nextRound > 7 {
+			nextRound = 1
+		}
 
-  })
+		c.HTML(200, "roundPlay.tmpl", gin.H{
+			"round":       testFunction(),
+			"description": rounds[i-1].description,
+			"contract":    rounds[i-1].contract,
+			"nextRound":   nextRound,
+		})
+	})
 
-  r.Run()
+	r.Run()
 }
---
+++ testFunction.go
@@ -0,0 +1,5 @@
+package main
+
+func testFunction() int32  {
+  return 12
+}
