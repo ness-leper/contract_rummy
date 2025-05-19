623af9cab50dcd5c7dd68ea92502c9896602d850
nick <nmonforte@gmail.com>
2025-05-19 09:52:59 -0400

    fixed the nextround and current picker

--- app/rounds/rounds.go
+++ app/rounds/rounds.go
@@ -13,13 +13,13 @@ type Round struct {
 
 func GetRound(round string) Round {
 	var rounds = []Round{
-		{"Deal 10 cards", "2 Sets", 1},
-		{"Deal 10 cards", "1 Set and 1 Sequence", 2},
-		{"Deal 10 cards", "2 Sequence's", 3},
-		{"Deal 10 cards", "3 Sets", 4},
-		{"Deal 12 cards", "2 Sets and 1 Sequence", 5},
-		{"Deal 12 cards", "1 Set and 2 Sequence's", 6},
-		{"Deal 12 cards", "Three Sequence's", 7},
+		{"Deal 10 cards", "2 Sets", 2},
+		{"Deal 10 cards", "1 Set and 1 Sequence", 3},
+		{"Deal 10 cards", "2 Sequence's", 4},
+		{"Deal 10 cards", "3 Sets", 5},
+		{"Deal 12 cards", "2 Sets and 1 Sequence", 6},
+		{"Deal 12 cards", "1 Set and 2 Sequence's", 7},
+		{"Deal 12 cards", "Three Sequence's", 1},
 	}
 
 	nextRound := 0
@@ -34,7 +34,7 @@ func GetRound(round string) Round {
 	}
 
 	fmt.Print("\n\n\n")
-	fmt.Println("The Next round is", rounds[i].NextRound)
+	fmt.Println("The Next round is", rounds[i-1].NextRound)
 	fmt.Println("The current round is", i)
 	fmt.Print("\n\n\n")
 	return rounds[i-1]
