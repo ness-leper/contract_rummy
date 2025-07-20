1bb6d3ca8a7a2677274cf70e334678a23364efc9
nick <nmonforte@gmail.com>
2025-07-20 10:37:57 -0400

    basic

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -8,9 +9,20 @@ import (
 
 func main() {
 	app := fiber.New()
+	app.Static("./templates/", "templates")
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Fiber!")
+		return c.SendFile("./templates/index.tmpl")
+	})
+
+	app.Get("/round", func(c *fiber.Ctx) error {
+		fmt.Println("We were here")
+		return c.JSON(fiber.Map{"message":"We were here"})
+	})
+
+	app.Get("/round/:id", func(c *fiber.Ctx) error {
+		idStr := c.Params("id")
+		return c.JSON(fiber.Map{"message":"We were here " + idStr})
 	})
 
 	app.Get("/api/users/:id", func(c *fiber.Ctx) error {
